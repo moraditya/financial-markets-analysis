874ab14a088f3c8a4d04474a35cad83dfe079d73
moraditya <mor.aditya.college@gmail.com>
2024-09-19 20:43:35 -0700

    Calculated percentage change of a stock on any given day

--- sql/basic_calculated_metrics.sql
+++ sql/basic_calculated_metrics.sql
@@ -11,3 +11,18 @@ SET daily_price_change = s."Close" - s."Open"
 FROM stocks s
 WHERE stocks_indicators.symbol = s.symbol
 AND stocks_indicators."Date" = s."Date";
+
+
+
+
+--- Percentage Change 
+-- [((close - open)/open)*100]
+-- Step 1: Add percentage_change to stocks_indicators
+ALTER TABLE stocks_indicators ADD COLUMN percentage_change DOUBLE PRECISION;
+
+-- Step 2: Calculate percentage change
+UPDATE stocks_indicators
+SET percentage_change = ((s."Close" - s."Open") / s."Open") * 100
+FROM stocks s
+WHERE stocks_indicators.symbol = s.symbol
+AND stocks_indicators."Date" = s."Date";
