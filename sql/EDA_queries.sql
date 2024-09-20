1e8f1d24cd5f93a688f64635176871a97e2ef631
moraditya <mor.aditya.college@gmail.com>
2024-09-20 01:55:28 -0700

    created daily, weekly and monthly aggregated data

--- sql/EDA_queries.sql
+++ sql/EDA_queries.sql
@@ -1,1 +1,35 @@
 -- Exploratory Data Analysis Queries
+
+
+--- Cleaned and aggregated tables (daily, weekly, and monthly)
+-- Daily Aggregated Table
+CREATE TABLE daily_aggregated_data AS
+SELECT 
+    symbol,
+    "Date",
+    "Close",
+    "Volume"
+FROM stocks
+WHERE "Close" IS NOT NULL;
+
+-- Weekly Aggregated Table
+CREATE TABLE weekly_aggregated_data AS
+SELECT 
+    symbol,
+    DATE_TRUNC('week', "Date") AS week_start,
+    AVG("Close") AS weekly_avg_close,
+    SUM("Volume") AS weekly_volume
+FROM stocks
+GROUP BY symbol, DATE_TRUNC('week', "Date")
+ORDER BY symbol, week_start;
+
+-- Monthly Aggregated Table
+CREATE TABLE monthly_aggregated_data AS
+SELECT 
+    symbol,
+    DATE_TRUNC('month', "Date") AS month_start,
+    AVG("Close") AS monthly_avg_close,
+    SUM("Volume") AS monthly_volume
+FROM stocks
+GROUP BY symbol, DATE_TRUNC('month', "Date")
+ORDER BY symbol, month_start;
