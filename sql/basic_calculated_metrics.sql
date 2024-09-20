a3ac0664307cce92c743070c6901d0196bc81668
moraditya <mor.aditya.college@gmail.com>
2024-09-20 01:29:28 -0700

    Added more basic metrics: price change (high-low difference), average price, and volume weighted average price to the file

--- sql/basic_calculated_metrics.sql
+++ sql/basic_calculated_metrics.sql
@@ -26,3 +26,48 @@ SET percentage_change = ((s."Close" - s."Open") / s."Open") * 100
 FROM stocks s
 WHERE stocks_indicators.symbol = s.symbol
 AND stocks_indicators."Date" = s."Date";
+
+
+
+
+
+--- Price Change (High-low Difference)
+-- Step 1: Add price_range to stocks_indicators
+ALTER TABLE stocks_indicators ADD COLUMN price_range DOUBLE PRECISION;
+
+-- Step 2: Calculate the price range
+UPDATE stocks_indicators
+SET price_range = s."High" - s."Low"
+FROM stocks s
+WHERE stocks_indicators.symbol = s.symbol
+AND stocks_indicators."Date" = s."Date";
+
+
+
+
+
+--- Average Price
+-- Step 1: Add average_price to stocks_indicators
+ALTER TABLE stocks_indicators ADD COLUMN average_price DOUBLE PRECISION;
+
+-- Step 2: Calculate the average price
+UPDATE stocks_indicators
+SET average_price = (s."Open" + s."Close") / 2
+FROM stocks s
+WHERE stocks_indicators.symbol = s.symbol
+AND stocks_indicators."Date" = s."Date";
+
+
+
+
+
+--- Volume-Weighted Average Price
+-- Step 1: Add vwap to stocks_indicators
+ALTER TABLE stocks_indicators ADD COLUMN vwap DOUBLE PRECISION;
+
+-- Step 2: Calculate VWAP
+UPDATE stocks_indicators
+SET vwap = ((s."High" + s."Low" + s."Close") / 3) * s."Volume"
+FROM stocks s
+WHERE stocks_indicators.symbol = s.symbol
+AND stocks_indicators."Date" = s."Date";
